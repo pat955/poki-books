ec78480ac7255795e53877cd9a2718aff6e68c00
pat955 <patricijap8@gmail.com>
2023-10-04 20:30:28 +0200

    Added most used words function, made the printing prettier

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from nltk import tokenize
 
 def main():
     frank_path = 'books/frankenstein.txt'
-    keyword = 'present'
-    search_finds = search(frank_path, keyword)
-    print(f'The word {keyword} was found here:')
-    for sentence, instance in search_finds.items():
-        print(f'{keyword.} was found here:\n{sentence}\nAt line {instance}.\n')
+    #keyword = 'present'
+    #search_finds = search(frank_path, keyword)
+    #print(f'The word {keyword} was found here:')
+    #for sentence, instance in search_finds.items():
+    #    print(f'{keyword} was found here:\n{sentence}\nAt line {instance}.\n')
 
-    # print(report(frank_path))
+    print('Top 20 most used words:')
+    for word, value in most_used_words(frank_path, 20).items():
+        print(f'{word} '+ '-'*(7-len(word))+f'was found {value} times!')
+
+    print(report(frank_path))
     # print(count_words(frank_path))
     # print(get_book_text(frank_path))
 
@@ -20,6 +24,7 @@ def get_book_text(path):
 def count_words(path):
     book_text = get_book_text(path)
     return len(book_text.split(' '))
+    
 
 def count_characters(path):
     count_dict = {}
@@ -49,15 +54,15 @@ def count_characters(path):
 
 
 def report(path):
-    report_text = f'---| Book report of {path} |---\n'+ f'Total words: {count_words(path)}\n'
+    report_text = ('-'*10 + f'| Book report of {path} |---'+'-'*10+'\n'+ f'Total words: {count_words(path)}\n')
 
     sorted_chr_dict = dict(sorted(count_characters(path).items(), key=lambda item: item[1], reverse= True))
     filtered_dict = dict(filter(lambda item: item[0].isalpha(), sorted_chr_dict.items()))
 
     for character, amount in filtered_dict.items():
-        report_text += (f'The character {character} appears {amount} times.\n')
+        report_text += ('-'*(10) + f'The character {character} appears {amount} times.'+'-'*(13 -len(str(amount)))+'\n')
 
-    report_text += ('---| End of book report |---')
+    report_text += ('-'*15 +'| End of book report |'+ '-'*15)
     return report_text
 
 def search(path, keyword):
@@ -70,7 +75,7 @@ def search(path, keyword):
 
         for word in sentence.split(' '):
             
-            word = word.strip(',,\n ;:."[]()*\\n')
+            word = word.strip(',\n ;:."[]()*\@{}`\'12345679_=€$£&%#! ')
 
             if word.lower() == keyword.lower():
                 
@@ -80,6 +85,30 @@ def search(path, keyword):
 
     return snt_with_keyword
 
-
+def most_used_words(path, num):
+    wordlist = {}
     
+    for word in get_book_text(path).split(' '):
+        word = word.strip(',\n ;:."[]()*\@{}`\'12345679_=€$£&%#! ')
+        if word == '':
+            continue
+        if word in wordlist:
+            wordlist[word] += 1
+        else:
+            wordlist[word] = 1
+
+    wordlist = dict(sorted(wordlist.items(), key=lambda item: item[1], reverse= True))
+    top_words = {}
+    count = 0
+
+    for key, value in wordlist.items():
+        if count == num:
+            break 
+        top_words[key] = value
+        count += 1
+
+    return top_words
+
+
+
 main()
