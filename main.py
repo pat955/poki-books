45940faca9d8f25626ce5cf356e1a58a32c63ec8
pat955 <patricijap8@gmail.com>
2023-11-06 11:47:04 +0100

    Cleaned up some code including word_count. Planning on making a upload folder so a user can actually use this

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import re
 from nltk import tokenize
 
 def main():
-    frank_path = 'books/frankenstein.txt'
-    #keyword = 'present'
-    #search_finds = search(frank_path, keyword)
-    #print(f'The word {keyword} was found here:')
-    #for sentence, instance in search_finds.items():
-    #    print(f'{keyword} was found here:\n{sentence}\nAt line {instance}.\n')
+    #print('Put all text files in bookbot folder before starting program.')
+    #book_path = input('Name of book with (ex. frankenstein.txt): ')
+    book_path = 'books/frankenstein.txt'
 
-    print('Top 20 most used words:')
-    for word, value in most_used_words(frank_path, 20).items():
+    print('Top 10 most used words:')
+    for word, value in most_used_words(book_path, 10).items():
         print(f'{word} '+ '-'*(7-len(word))+f'was found {value} times!')
 
-    print(report(frank_path))
+    print(report(book_path))
     # print(count_words(frank_path))
     # print(get_book_text(frank_path))
+
+def give_bookpath_name(path):
+    pass 
+
+def add_book_to_folder(path):
+    pass 
+
+def get_list_of_books():
+    pass 
 
 def get_book_text(path):
     book = open(path, 'r')
@@ -24,32 +30,24 @@ def get_book_text(path):
 def count_words(path):
     book_text = get_book_text(path)
     return len(book_text.split(' '))
-    
 
 def count_characters(path):
     count_dict = {}
     book_text = get_book_text(path)
 
-    for i in book_text:
-      
-        if i.isalpha():
-            i = i.lower()
+    for word in book_text:
+        if word.isalpha():
+            if word.lower() not in count_dict:
+                count_dict[word.lower()] = 0
+            count_dict[word.lower()] += 1
 
-            if i in count_dict:
-                count_dict[i] += 1
-
-            else:
-                count_dict[i] = 1
-
-        elif i == ' ' or i == '\t' or i == '\n':
+        elif word == ' ' or word == '\t' or word == '\n':
             continue
 
         else:
-            if i in count_dict:
-                count_dict[i] += 1 
-            else:
-                count_dict[i] = 1      
-
+            if word not in count_dict:
+                count_dict[word] = 0 
+            count_dict[word] += 1      
     return count_dict
 
 
@@ -81,21 +79,21 @@ def search(path, keyword):
                 
                 snt_with_keyword[sentence] = line_num
 
-     
-
     return snt_with_keyword
-
-def most_used_words(path, num):
+def get_wordlist(path):
     wordlist = {}
     
     for word in get_book_text(path).split(' '):
         word = word.strip(',\n ;:."[]()*\@{}`\'12345679_=€$£&%#! ')
         if word == '':
             continue
-        if word in wordlist:
-            wordlist[word] += 1
-        else:
-            wordlist[word] = 1
+        if word not in wordlist:
+            wordlist[word] = 0
+        wordlist[word] += 1
+    return wordlist
+
+def most_used_words(path, num):
+    wordlist = get_wordlist(path)
 
     wordlist = dict(sorted(wordlist.items(), key=lambda item: item[1], reverse= True))
     top_words = {}
@@ -108,7 +106,6 @@ def most_used_words(path, num):
         count += 1
 
     return top_words
-
 
 
 main()
