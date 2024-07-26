ab54dcc3287d706eb2403120ffd8ed6ba75944f1
pat955 <patricija.pivoraite@gmail.com>
2024-07-26 10:58:24 +0200

    fetched

--- src/help_menu.py
+++ src/help_menu.py
@@ -4,4 +4,5 @@ def info(window):
     window.text_frame.insert('This is my first actual coding project so i apologize for any bugs!\nUpload txt files and read them, change theme and enter fullscreen.\nMy GitHub: @pat955')
 
 def contact():
+    
     pass
