7fc691a4d7c32e850621e105e14015c5c737fe5a
pat955 <patricija.pivoraite@gmail.com>
2024-08-26 11:15:57 +0200

    basic html functionality

--- src/tkinter_html.py
+++ src/tkinter_html.py
@@ -1,17 +1,16 @@
-import re
 from bs4 import BeautifulSoup
 
 def parse_html(text_frame, text):
+    
     soup = BeautifulSoup(text, 'html.parser')
-    # tags = ['h1', 'italic', 'bold']
-    # add title
     try:
         insert(text_frame, title(soup), 'h1')
     except AttributeError:
         print('No title error')
     contents_r(text_frame, soup)
-    
+
 def contents_r(text_frame, soup):
+    
     print(soup.find_all())
     for tag in soup.find_all():
         try:
@@ -34,3 +33,6 @@ def insert(text_frame, text, tag=None):
 
 def append(text_frame, text, tag=None):
     text_frame.txt.append(text, tag=tag, add_newline=True)
+
+def pagebreak():
+    pass
